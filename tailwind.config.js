7c8dd286c95d9486b88753cb1872efdd0295897d
DO274 <mayadaamin808@gmail.com>
2024-11-05 20:18:04 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  container: {
+    center: true,
+  },
+  extend: {},
+};
+export const plugins = [];
+
